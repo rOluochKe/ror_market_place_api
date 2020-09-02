9d1aaea5a0e3be88ce6b963b6326ccbb0e570947
Raymond Oluoch <rasholuoch@gmail.com>
2020-09-02 23:59:10 +0300

    Associates products and orders with a placements model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+  has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
-
-  has_many :products, dependent: :destroy
-  has_many :orders, dependent: :destroy
 
   has_secure_password
 end
